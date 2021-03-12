9b828dc088ba1b4deae1f071cd744d1c2929df1b
Claudio Pereira <claudiofrancopereira@gmail.com>
2021-03-12 12:54:31 -0300

    Added TYPEORM and created CPF table

---
+++ backend/src/database/migrations/1615563949573-create_cpf_table.ts
@@ -0,0 +1,50 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createCpfTable1615563949573 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable( new Table({
+      name: 'cpf',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true,
+        },
+
+        {
+          name: 'cpf_number',
+          type: 'varchar'
+        },
+        
+        {
+          name: 'name',
+          type: 'varchar'
+        },
+        
+        {
+          name: 'address',
+          type: 'varchar'
+        },
+        
+        {
+          name: 'contact',
+          type: 'varchar'
+        },
+
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'now()',
+        },
+      ]
+    }));
+
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('cpf');
+    
+  }
+
+}
