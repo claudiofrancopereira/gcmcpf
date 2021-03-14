8edaf1ac529a4a195603398727a69acdc60ef80e
Claudio Pereira <claudiofrancopereira@gmail.com>
2021-03-14 20:33:06 -0300

    Fixed compiler options

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
 import "reflect-metadata";
 
-import express from 'express';
 import 'dotenv/config';
+import express from 'express';
+
 import './database/connection';
 
 import cors from 'cors';
@@ -14,4 +15,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
