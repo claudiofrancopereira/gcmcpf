15d5d13335b32a8e5f4690508c3b128b0430155a
Claudio Pereira <claudiofrancopereira@gmail.com>
2021-04-18 20:22:03 -0300

    Beta 1

--- backend/src/views/cpf_viewss.ts
+++ backend/src/views/cpf_viewss.ts
@@ -5,6 +5,7 @@ import abordagemsView from './abordagems_view';
 export default {
     render(cpf: CPF) {
         return {
+            id: cpf.id,
             name: cpf.name,
             cpf_number: cpf.cpf_number,
             address: cpf.address,
