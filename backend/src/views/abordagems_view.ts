0246ba0d1f6e75df1e792afbdd43bf13cc49c28d
Claudio Pereira <claudiofrancopereira@gmail.com>
2021-04-19 00:38:03 -0300

    Date/Time fixing

--- backend/src/views/abordagems_view.ts
+++ backend/src/views/abordagems_view.ts
@@ -4,7 +4,7 @@ export default {
     render(abordagem: Abordagem) {
         return {
             date: abordagem.created_at.toLocaleDateString('pt-BR'),
-            time: abordagem.created_at.toLocaleTimeString('pt-BR', { timeZone: 'America/Sao_Paulo ', hour12: false }),
+            time: abordagem.created_at.toLocaleTimeString('pt-BR', { timeZone: 'America/Sao_Paulo', hour12: false }),
             location: abordagem.location,
             gcm: abordagem.gcm,
             obs: abordagem.obs,
